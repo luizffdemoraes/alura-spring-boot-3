288e5b71b23cc91543bfcd355fcb2b43ae9e7388
Luiz Felipe <lffm1994@gmail.com>
2022-12-02 01:17:37 -0300

    Entidades JPA

---
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -0,0 +1,26 @@
+package med.voll.api.medico;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "medicos")
+@Entity(name = "Medico")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Medico {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String crm;
+    @Enumerated(EnumType.STRING) // Fazer com que façam parte da mesma tabela mesmo em classes separadas
+    private Especialidade especialidade;
+    @Embedded
+    private Endereco endereco;
+}
