dc1a742a806623cd45990ea4481e8b05cc2abdda
Luiz Felipe <lffm1994@gmail.com>
2022-12-03 00:51:02 -0300

    Validaçao com Bean Validation

--- src/main/java/med/voll/api/medico/DadosCadastroMedico.java
+++ src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -1,4 +1,23 @@
 package med.voll.api.medico;
 
-public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade, DadosEndereco endereco) {
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+public record DadosCadastroMedico(
+        @NotBlank
+        String nome,
+        @NotBlank
+        @Email
+        String email,
+
+        @NotBlank
+        @Pattern(regexp = "\\d{4,6}")
+        String crm,
+        @NotNull
+        Especialidade especialidade,
+
+        @NotNull @Valid DadosEndereco endereco) {
 }
