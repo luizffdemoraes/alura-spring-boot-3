7b63d6291f7aa6889076de272d3edad45962cd50
Luiz Felipe <lffm1994@gmail.com>
2022-12-04 22:23:06 -0300

    Atualizando dados

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -18,7 +18,7 @@ public class Medico {
     private Long id;
     private String nome;
     private String email;
-    private  String telefone;
+    private String telefone;
     private String crm;
     @Enumerated(EnumType.STRING) // Fazer com que façam parte da mesma tabela mesmo em classes separadas
     private Especialidade especialidade;
@@ -32,5 +32,17 @@ public class Medico {
         this.crm = dados.crm();
         this.especialidade = dados.especialidade();
         this.endereco = new Endereco(dados.endereco());
+    }
+
+    public void atualizarInformacoes(DadosAtualizacaoMedico dados) {
+        if (dados.nome() != null) {
+            this.nome = dados.nome();
+        }
+        if (dados.telefone() != null) {
+            this.telefone = dados.telefone();
+        }
+        if (dados.endereco() != null) {
+            this.endereco.atualizarInformacoes(dados.endereco());
+        }
     }
 }
