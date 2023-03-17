070f18f10e89a9aa63efcea7382183f27f5e029a
Luiz Felipe <lffm1994@gmail.com>
2023-03-16 22:23:02 -0300

    Escolha de médico aleatório

--- src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
+++ src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
@@ -1,6 +1,7 @@
 package med.voll.api.domain.consulta;
 
 import med.voll.api.domain.exception.ValidacaoException;
+import med.voll.api.domain.medico.Medico;
 import med.voll.api.domain.medico.MedicoRepository;
 import med.voll.api.domain.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,12 +28,21 @@ public class AgendaDeConsultas {
             throw new ValidacaoException("Id do médico informado não existe!");
         }
 
-        var medico = medicoRepository.findById(dados.idMedico()).get();
-        var paciente = escolherMedico(dados);
+        var paciente = pacienteRepository.getReferenceById(dados.idMedico());
+        var medico = escolherMedico(dados);
         var consulta = new Consulta(null, medico, paciente, dados.data());
         consultaRepository.save(consulta);
     }
 
     private Medico escolherMedico(DadosAgendamentoConsulta dados) {
+        if (dados.idMedico() != null) {
+            return medicoRepository.getReferenceById(dados.idMedico());
+        }
+
+        if (dados.especialidade() == null) {
+            throw new ValidacaoException("Especialidade é obrigatória quando médico não for escolhido!");
+        }
+
+        return medicoRepository.escolherMedicoAleatorioLivreNaData(dados.especialidade(), dados.data());
     }
 }
